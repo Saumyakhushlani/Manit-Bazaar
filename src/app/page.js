5469fa86bf5e05df2e001d02c79f235adb91e1d6
Saumya khushlani <saumyakhushlani9@gmail.com>
2025-07-19 01:26:30 +0530

    Teams page added

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,11 @@
 import NavbarDemo from "@/components/Header";
+import { LandingPage } from "@/components/Landing";
 
 export default function Home() {
   return (
     <div className="flex flex-col justify-center items-center ">
       <NavbarDemo/>
+      <LandingPage/>
     </div>
   );
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -23,8 +23,8 @@ export default function NavbarDemo() {
       link: "/products",
     },
     {
-      name: "Team",
-      link: "/team",
+      name: "About",
+      link: "/about",
     },
   ];
 
