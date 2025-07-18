70fcc99433a2c39f6df65e66e017a55e1b8fe5cf
Aadi Jain <aadijaintikamgarh@gmail.com>
2025-07-19 01:43:03 +0530

    add dbconfig and add category, product, user model

---
+++ src/Models/user.js
@@ -0,0 +1,43 @@
+import mongoose, { Schema } from "mongoose";
+import jwt from "jsonwebtoken";
+
+const userSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      lowercase: true,
+    },
+    phone: {
+      type: Number,
+      required: true,
+    },
+    products: [
+      {
+        type: mongoose.Types.ObjectId,
+        ref: "product",
+      },
+    ],
+    refreshToken: String,
+    profilePhoto: String,
+  },
+  { timestamps: true }
+);
+
+user.methods.generateRefreshToken = async () => {
+  const token = jwt.sign(
+    { id: this._id, name: this.name },
+    `${process.env.REFRESH_TOKEN_SECRET}`
+  );
+
+  return token
+};
+
+const user = mongoose.models.user || mongoose.model("user",userSchema );
+
+export default user;
