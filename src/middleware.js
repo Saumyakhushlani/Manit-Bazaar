a00b0c509d0726e3ded80a126db31146410c3a91
Aadi Jain <aadijaintikamgarh@gmail.com>
2025-07-21 01:50:01 +0530

    add middleware

--- src/middleware.js
+++ src/middleware.js
@@ -2,16 +2,14 @@ import { NextResponse } from "next/server";
 import { cookies } from "next/headers";
 
 export async function middleware(req) {
-//   const response = NextResponse.next();
-//   const token = (await cookies()).get("token")?.value;
-//   const path = req.nextUrl.pathname;
+  const token = (await cookies()).get("token")?.value;
+  const path = req.nextUrl.pathname;
 
-//   if (token && path === "/login") {
-//     return NextResponse.redirect(new URL("/", req.url));
-//   }
+  if (!token && path.startsWith("/userDashboard")) {
+    return NextResponse.redirect(new URL("/login", req.url));
+  }
 
-//   if (!token && path !== "/login") {
-//     return NextResponse.redirect(new URL("/login", req.url));
-//   }
-
+  if (token && path.startsWith("/login")) {
+    return NextResponse.redirect(new URL("/", req.url));
+  }
 }
