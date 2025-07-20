2c88b17aacdb04a65eda93644c65da10a543210e
Saumya khushlani <saumyakhushlani9@gmail.com>
2025-07-20 16:58:36 +0530

    User Dashboard Completd

--- src/app/userDashboard/page.jsx
+++ src/app/userDashboard/page.jsx
@@ -1,7 +1,7 @@
 import React from "react";
-// import axios from "axios";
-import UserProducts from "@/components/UserProducts";
-// import { getUserId } from "@/helper/getUserId";
+import NavbarDemo from "@/components/Header";
+import { MdOutlineErrorOutline } from "react-icons/md";
+
 
 const page = () => {
   const data = [
@@ -16,7 +16,7 @@ const page = () => {
       createdAt: "2025-07-01T10:15:00Z",
       owner: "64c9a2b1f1e3ab45d7e67890",
       updatesAt: "2025-07-10T12:00:00Z",
-    },{
+    }, {
       id: "p001",
       name: "Wireless Headphones",
       description:
@@ -41,39 +41,49 @@ const page = () => {
       updatesAt: "2025-07-10T12:00:00Z",
     },
   ];
-  //   const userId = getUserId();
-  //   const fetchProduct = async () => {
-  //     try {
-  //       const response = await axios.get("/api/:userId/product");
-  //       return response;
-  //     } catch (error) {}
-  //   };
+
+  const User = {
+    name: "Saumya",
+    email: "saumyakhushlani9@gmail.com",
+    img: "/saumya.jpg",
+    phone: "+91 9929335307"
+  }
 
   return (
-    <div>
-      <nav className="flex flex-row justify-between p-5 sm:p-10">
-        <span className="text-2xl font-semibold">Dashboard</span>
-        <button className="bg-purple-500 px-5 py-1 rounded-full cursor-pointer hover:opacity-80">
-          Add Product
-        </button>
-      </nav>
-      {/* your Products */}
-      <div className="border p-4 mx-3 rounded-md">
-        <div className="flex ">
-          <h1 className="sm:px-10 px-5 font-semibold text-lg">Your Products</h1>
+    <>
+      <NavbarDemo />
+      <div className="bg-neutral-950 flex flex-col justify-center items-center py-15 px-10">
+        <div className="flex md:flex-row flex-col justify-center items-center gap-10 bg-neutral-900 my-5 rounded-xl p-6">
+          <img src={User.img} alt={User.name} className="w-40 h-40 rounded-full border-2 border-purple-500 flex-1" />
+          <div className="flex-3">
+            <div className="text-3xl text-purple-500 font-semibold mb-2">Name: {User.name}</div>
+            <div className="text-xl text-white">Email: {User.email}</div>
+            <div className="text-xl text-white">Phone :{User.phone}</div>
+          </div>
+
         </div>
-        <div className="text-white">
-          {data.map((d, index) => (
-            <UserProducts
-              key={index}
-              name={d.name}
-              price={d.price}
-              image={d.image}
-            />
-          ))}
-        </div>
+        <div className="text-4xl text-purple-500 font-semibold mt-4 mb-2">My Products</div>
+        <button className="bg-purple-500 px-4 py-2 rounded-lg mb-10">Add New Item</button>
+        {data.length > 0 ?
+          <div className="flex flex-row flex-wrap justify-evenly items-center  gap-10">
+            {data.map((e) => (
+              <div key={e.id} className="rounded-lg w-75 bg-neutral-900 pb-3">
+                <img src={e.image} alt={e.name}  className="w-full h-60 object-cover"/>
+                <div className="text-purple-500 text-2xl mt-1.5 font-semibold px-2">{e.name}</div>
+                <div className="text-white text-md my-1 px-2">{e.description}</div>
+                <div className="text-white font-semibold px-2">₹{e.price}</div>
+                <button className=" mx-2 bg-red-600 text-white w-30 mt-2 rounded-lg py-1">Delete</button>
+              </div>
+            ))}
+          </div>
+          :
+          <div className="flex flex-col items-center justify-center min-h-20">
+            <MdOutlineErrorOutline size={150} className="text-purple-500 " />
+            <div className="text-2xl mt-3 text-white ">It looks like you haven’t listed anything yet.</div>
+          </div>
+        }
       </div>
-    </div>
+    </>
   );
 };
 
