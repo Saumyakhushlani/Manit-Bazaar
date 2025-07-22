6a0997a2053a33ff26773ed2a17326e813327db1
Saumya khushlani <saumyakhushlani9@gmail.com>
2025-07-22 14:12:41 +0530

    Aurorra removed

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -36,7 +36,7 @@ const ProductCard = ({ data }) => {
                         initial={{ opacity: 0 }}
                         animate={{ opacity: 1 }}
                         exit={{ opacity: 0 }}
-                        className='fixed inset-0 bg-black/60 backdrop-blur-sm flex justify-center items-center z-50 p-4 '
+                        className='fixed inset-0 bg-black/60 backdrop-blur-sm flex justify-center items-center  p-4 z-1000'
                         onClick={closeModal}
                     >
                         <motion.div
